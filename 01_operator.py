7d36d57b30fda95eee28b4cc4e04b5f8909ed4a7
Prashant Rai <raiprashant9935@gmail.com>
2024-12-04 23:15:07 +0530

    myfirst

---
+++ 01_operator.py
@@ -0,0 +1,30 @@
+name = "Prashant" #string
+age = 22
+print(name)
+print("My name is :", name)
+print("My age is :", age)
+print(type(name))
+print(type(age))
+old = True
+print(type(old))
+
+#Program 2  Realational operator
+#check if a = b 
+a = 50
+b = 20
+print(a  == b)
+
+#Program 3
+a = 50
+b = 20
+print(a  != b)
+print(a < b)
+
+#Program 4 Assignment operator
+num = 10
+num = num + 10
+print("num : ", num)
+
+#Logical operator( and , or, not)
+print(not False)
+print(not True)
